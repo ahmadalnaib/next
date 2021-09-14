57e35b4439976e6c8dda313437cd8186e9b98b07
ahmedhabeeb73 <alnaib888@gmail.com>
2021-09-14 19:10:11 +0200

    add module aliases with jsconfig

--- job-frontend/components/Layout.js
+++ job-frontend/components/Layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
-import stytes from '../styles/Layout.module.css'
+import stytes from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
   return (
